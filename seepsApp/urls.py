b89f42aab20b49da59e703248a7ec185bbdd9266
AMANI-BLU <solomonamanuel66@gmail.com>
2024-04-25 18:19:17 +0300

    password reset

--- seepsApp/urls.py
+++ seepsApp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', login_view,name ="login_view" ),
     path('home/', home,name ="home" ),
@@ -58,7 +58,24 @@ urlpatterns = [
 
 
 
-
+ path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='password_reset.html',
+        email_template_name='password_reset_email.html',
+        success_url='/password_reset/done/'
+    ), name='password_reset'),
+    
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='password_reset_done.html'
+    ), name='password_reset_done'),
+    
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password_reset_confirm.html',
+        success_url='/reset/done/'
+    ), name='password_reset_confirm'),
+    
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password_reset_complete.html'
+    ), name='password_reset_complete'),
 
 
 
